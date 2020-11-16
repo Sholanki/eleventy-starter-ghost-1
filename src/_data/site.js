b77447ad8e7e7b74974363948a3def01af5420ef
Sholanki <63105386+Sholanki@users.noreply.github.com>
2020-11-16 09:56:53 +0530

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,6 +2,15 @@ require("dotenv").config();
 
 const ghostContentAPI = require("@tryghost/content-api");
 
+const {titleCase} = require("title-case");
+
+// This regex finds all wikilinks in a string
+const wikilinkRegExp = /\[\[([\w\s/-]+)(.\w+)?\s?(\|\s?([\w\s/]+))?\]\]/g
+
+function caselessCompare(a, b) {
+    return a.toLowerCase() === b.toLowerCase();
+}
+
 // Init Ghost API
 const api = new ghostContentAPI({
   url: process.env.GHOST_API_URL,
@@ -22,4 +31,48 @@ module.exports = async function() {
   if (process.env.SITE_URL) siteData.url = process.env.SITE_URL;
 
   return siteData;
+};
+
+module.exports = {
+    layout: "post2.html",
+    type: "notes",
+    eleventyComputed: {
+        title: data => titleCase(data.title || data.page.fileSlug),
+        backlinks: (data) => {
+            const notes = data.collections.notes;
+            const currentFileSlug = data.page.fileSlug;
+
+            let backlinks = [];
+
+            // Search the other notes for backlinks
+            for(const otherNote of notes) {
+                const noteContent = otherNote.template.frontMatter.content;
+
+                // Get all links from otherNote
+                const outboundLinks = (noteContent.match(wikilinkRegExp) || [])
+                    .map(link => (
+                        // Extract link location
+                        link.slice(2,-2)
+                            .split("|")[0]
+                            .replace(/[^\w\s/-]+/g,'')
+                            .replace(/.(md|markdown)\s?$/i, "")
+                    ));
+
+                // If the other note links here, return related info
+                if(outboundLinks.some(link => caselessCompare(link, currentFileSlug))) {
+
+                    // Construct preview for hovercards
+                    let preview = noteContent.slice(0, 240);
+
+                    backlinks.push({
+                        url: otherNote.url,
+                        title: otherNote.data.title,
+                        preview
+                    })
+                }
+            }
+
+            return backlinks;
+        }
+    }
 };
