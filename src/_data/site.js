0d7f84b16a69fe3a938428fa3f7725f4d6d19724
Sholanki <63105386+Sholanki@users.noreply.github.com>
2020-11-16 10:12:39 +0530

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -33,3 +33,46 @@ module.exports = async function() {
   return siteData;
 };
 
+module.exports = {
+    layout: "post2.html",
+    type: "notes",
+    eleventyComputed: {
+        title: data => titleCase(data.title || data.page.fileSlug),
+        backlinks: (data) => {
+            const notes = data.collections.notes;
+            const currentFileSlug = data.page.fileSlug;
+
+            let backlinks = [];
+
+            // Search the other notes for backlinks
+            for(const otherNote of notes) {
+                const noteContent = otherNote.template.frontMatter.content;
+
+                // Get all links from otherNote
+                const outboundLinks = (noteContent.match(wikilinkRegExp) || [])
+                    .map(link => (
+                        // Extract link location
+                        link.slice(2,-2)
+                            .split("|")[0]
+                            .replace(/[^\w\s/-]+/g,'')
+                            .replace(/.(md|markdown)\s?$/i, "")
+                    ));
+
+                // If the other note links here, return related info
+                if(outboundLinks.some(link => caselessCompare(link, currentFileSlug))) {
+
+                    // Construct preview for hovercards
+                    let preview = noteContent.slice(0, 240);
+
+                    backlinks.push({
+                        url: otherNote.url,
+                        title: otherNote.data.title,
+                        preview
+                    })
+                }
+            }
+
+            return backlinks;
+        }
+    }
+}
