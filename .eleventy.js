945230f6db45dfafa6bf2d778b90d72a56e88f44
Sholanki <63105386+Sholanki@users.noreply.github.com>
2021-02-12 11:21:16 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,16 +10,10 @@ const ghostContentAPI = require("@tryghost/content-api");
 const htmlMinTransform = require("./src/transforms/html-min-transform.js");
 
 // Init Ghost API
-const api = new ghostContentAPI({
-  url: process.env.GHOST_API_URL,
-  key: process.env.GHOST_CONTENT_API_KEY,
-  version: "v2"
-});
+
 
 // Strip Ghost domain from urls
-const stripDomain = url => {
-  return url.replace(process.env.GHOST_API_URL, "");
-};
+
 
 module.exports = function(config) {
   // Minify HTML
@@ -34,13 +28,7 @@ module.exports = function(config) {
   });
 
   // Copy images over from Ghost
-  config.addPlugin(localImages, {
-    distPath: "dist",
-    assetPath: "/assets/images",
-    selector: "img",
-    attribute: "data-src", // Lazy images attribute
-    verbose: false
-  });
+
 
   // Inline CSS
   config.addFilter("cssmin", code => {
@@ -59,7 +47,7 @@ module.exports = function(config) {
   });
 
   // Don't ignore the same files ignored in the git repo
-  config.setUseGitIgnore(false);
+
 
   // Get all pages, called 'docs' to prevent
   // conflicting the eleventy page object
@@ -85,100 +73,13 @@ module.exports = function(config) {
     return collection;
   });
 
-  // Get all posts
-  config.addCollection("posts", async function(collection) {
-    collection = await api.posts
-      .browse({
-        include: "tags,authors",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    collection.forEach(post => {
-      post.url = stripDomain(post.url);
-      post.primary_author.url = stripDomain(post.primary_author.url);
-      post.tags.map(tag => (tag.url = stripDomain(tag.url)));
-
-      // Convert publish date into a Date object
-      post.published_at = new Date(post.published_at);
-    });
-
-    // Bring featured post to the top of the list
-    collection.sort((post, nextPost) => nextPost.featured - post.featured);
-
-    return collection;
-  });
-
-  // Get all authors
-  config.addCollection("authors", async function(collection) {
-    collection = await api.authors
-      .browse({
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    // Get all posts with their authors attached
-    const posts = await api.posts
-      .browse({
-        include: "authors",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
+ 
     // Attach posts to their respective authors
-    collection.forEach(async author => {
-      const authorsPosts = posts.filter(post => {
-        post.url = stripDomain(post.url);
-        return post.primary_author.id === author.id;
-      });
-      if (authorsPosts.length) author.posts = authorsPosts;
-
-      author.url = stripDomain(author.url);
-    });
-
-    return collection;
-  });
-
-  // Get all tags
-  config.addCollection("tags", async function(collection) {
-    collection = await api.tags
-      .browse({
-        include: "count.posts",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
+   
 
     // Get all posts with their tags attached
-    const posts = await api.posts
-      .browse({
-        include: "tags,authors",
-        limit: "all"
-      })
-      .catch(err => {
-        console.error(err);
-      });
-
-    // Attach posts to their respective tags
-    collection.forEach(async tag => {
-      const taggedPosts = posts.filter(post => {
-        post.url = stripDomain(post.url);
-        return post.primary_tag && post.primary_tag.slug === tag.slug;
-      });
-      if (taggedPosts.length) tag.posts = taggedPosts;
-
-      tag.url = stripDomain(tag.url);
-    });
-
-    return collection;
-  });
+   
+  
 
   // Display 404 page in BrowserSnyc
   config.setBrowserSyncConfig({
